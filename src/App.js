0282d84ca06160f897325cd3442d20b7d01f70a1
1260215278 <39906816+1260215278@users.noreply.github.com>
2019-12-03 14:15:54 +0800

    react 提交
    测试玩玩

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+// import conson from './utils/conson'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Botton></Botton>
+        <Triangle></Triangle>
       </header>
     </div>
   );
 }
+
+
+class Triangle extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+    
+    }
+  }
+  render(){
+    return(
+          <div className="content"> </div>
+      )
+  }
+}
+
+class Botton extends React.Component{
+  constructor(props){
+      super(props)
+      this.state={
+        name:'Susan',
+        isState:false
+      }
+  }
+  
+  _click(){
+    console.log('这是一个点击事件')
+    this.setState({
+      name: this.state.isState ?  "xiaowu" : "lisi",
+      isState:!this.state.isState
+     }
+    )
+  }
+
+  render(){
+    // conson()
+    return (
+      <button  onClick={()=>this._click()}>我是一个{this.state.name}</button>
+    )
+  }
+}
+
 
 export default App;
---
+++ src/utils/conson.js
@@ -0,0 +1,13 @@
+// 花里胡哨的调试台
+function conson(){
+    let color=''
+    let  str =''
+    // 生成随机颜色 
+    // 生成随机字符串 随机字符可以更换成
+    setInterval(()=>{
+            color = '#'+Math.floor(Math.random()*0xffffff).toString(16);
+            str = '花里胡哨的调试台，颜色是'+'#'+Math.floor(Math.random()*0xffffff).toString(16);
+            console.log('%c%s', `padding: 2px 4px; width:300px;   text-align: center; background: ${color};color: white;border-radius: 5px;`, str);
+    },1000)
+}
+module.exports= conson
